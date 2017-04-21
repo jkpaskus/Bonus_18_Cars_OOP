364c7ce9e1970f2ae2748fbe03d08ca11de0ab9a
Jonas <jkpaskus@gmail.com>
2017-04-21 15:59:50 -0400

    First commit for rough draft of car class creation

---
+++ Bonus18/Car.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Car
+    {
+
+        #region variables
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+        #endregion
+
+        #region properties
+        public string CarMake
+        {
+            set
+            {
+              make = value;
+            }
+
+            get
+            {
+                return make;
+            }
+
+        }
+
+        public string CarModel
+        {
+            set
+            {
+                model = value;
+            }
+
+            get
+            {
+                return model;
+            }
+        }
+
+        public int CarYear
+        {
+            set
+            {
+                year = value;
+            }
+            get
+            {
+                return year;
+            }
+        }
+
+        public double CarPrice
+        {
+            set
+            {
+                price = value;
+            }
+            get
+            {
+                return price;
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region constructors
+        public Car()
+        {
+            make = "Ford";
+            model = "Fiesta";
+            year = 2016;
+            price = 20000.00;
+        }
+
+        public Car(string makeInput,
+            string modelInput,
+            int yearInput,
+            double priceInput)
+        {
+            make = makeInput;
+            model = modelInput;
+            year = yearInput;
+            price = priceInput;
+        }
+        #endregion 
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Bonus18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfCars;
+
+            Car Car1 = new Car();
+            Car Car2 = new Car("Chevy", "Impala", 1990, 16700.00);
+
+            Console.WriteLine("Welcome to the Grand Circus Motors admin console!");
+            Console.WriteLine("How many cars are you entering: ");
+            numOfCars = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("You are entering {0} cars", numOfCars);
+            Console.WriteLine($"Car make: {Car1.CarMake} Car Model: {Car1.CarModel} Make Year: {Car1.CarYear} Price: {Car1.CarPrice}");
+        }
+    }
+}
